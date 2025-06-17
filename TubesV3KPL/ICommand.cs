a67722f8900b6ed54c25ce3e70718e7fb7da8799
raatra10 <rafi180350@gmail.com>
2025-06-17 18:08:55 +0700

    Menerapkan clean code di class ICommand, RegiseterPelamarCommand, dan main

--- TubesV3KPL/ICommand.cs
+++ TubesV3KPL/ICommand.cs
@@ -4,7 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public interface ICommand
+using System;
+
+namespace TubesV3
 {
-    void Execute();
+    /// <summary> Kontrak untuk semua perintah. </summary>
+    // Interface dengan tanggung jawab tunggal
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+    /// <summary>
+    /// Membungkus Action menjadi perintah yang bisa dieksekusi.
+    /// </summary>
+    /// Mengubah Action menjadi implementasi command pattern
+    public class LambdaCommand : ICommand
+    {
+        private readonly Action _action;
+        public LambdaCommand(Action action) => _action = action;
+        public void Execute() => _action();
+    }
 }
